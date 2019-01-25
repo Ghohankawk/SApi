8dcc08a49aa78720a6558c1ff4eedd3cc392aedd
Ghohankawk <ghohank@gmail.com>
2019-01-25 15:17:13 +0800

    初始化代码

---
+++ config/base.config.js
@@ -0,0 +1,12 @@
+export default {
+	httpPort: 8989,
+	httpsPort: 9090,
+	ldap: {
+		use: false,
+		url: '',
+		username: '',
+		password: '',
+		group_base: '',
+		email_base: ''
+	}
+};
---
+++ config/db.config.js
@@ -0,0 +1,16 @@
+export default {
+	dev: {
+		host: 'localhost',
+		port: '27017',
+		username: '',
+		password: '',
+		database: 'SApi'
+	},
+	production: {
+		host: '10.138.10.166',
+		port: '27017',
+		username: '',
+		password: '',
+		database: 'SApi'
+	}
+};
